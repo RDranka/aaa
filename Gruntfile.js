b6f54838663e8f11abb8c922347d666a0c7df5f4
Imal perera <imaa95@gmail.com>
2019-11-02 15:41:09 +0530

    addding a sample code

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,8 @@ module.exports = function (grunt) {
     grunt.initConfig({
         uglify: {
             options: {
-                mangle: false
+                mangle: false,
+                sourceMap : true
             },
             minify_js: {
                 files: {
@@ -47,34 +48,53 @@ module.exports = function (grunt) {
             main: {
                 files: [
                     {cwd: 'build-output/_single-bundle', expand: true, src: ['TreineticEpubReader.js'], dest: 'dist/'},
-                    {cwd: 'dev/grunt_build', expand: true, src: ['index.html'], dest: 'dist/'},
-                    {cwd: 'dev/grunt_build', expand: true, src: ['Readme.md'], dest: 'dist/'},
+                    {cwd: 'dev/grunt_build', expand: true, src: ['index.html'], dest: 'dist/sample/'},
+                    {cwd: 'dev/grunt_build/css', expand: true, src: ['style.css'], dest: 'dist/sample/css'},
+                    {cwd: 'dev/grunt_build/js', expand: true, src: ['main.js'], dest: 'dist/sample/js'},
+                    {
+                        cwd: 'dev/grunt_build/assets',
+                        expand: true,
+                        src: ['images/**',],
+                        dest: './dist/sample/assets/'
+                    },
                     {
                         cwd: 'epub_content',
                         expand: true,
                         src: ['epub_2/**',],
-                        dest: './dist/epub_content/epub_2'
+                        dest: './dist/sample/assets/epub/'
                     },
                     {
                         cwd: 'epub_content',
                         expand: true,
                         src: ['epub_1.epub'],
-                        dest: './dist/epub_content/'
+                        dest: './dist/sample/assets/epub/'
                     },
                     {
                         cwd: 'build-output/',
                         expand: true,
                         src: ['deflate.js', 'inflate.js', 'z-worker.js'],
-                        dest: './dist/ZIPJS/'
+                        dest: './dist/workers/'
+                    },
+                    {
+                        cwd: 'build-output/',
+                        expand: true,
+                        src: ['deflate.js', 'inflate.js', 'z-worker.js'],
+                        dest: './dist/sample/assets/workers/'
                     },
                 ],
             },
+            copyToSample :{
+                files: [
+                    {cwd: 'dist', expand: true, src: ['TreineticEpubReader.css'], dest: 'dist/sample/css'},
+                    {cwd: 'dist', expand: true, src: ['TreineticEpubReader.js'], dest: 'dist/sample/js'},
+                ],
+            }
         },
     });
 
 
     //ZIPJS --> deflate.js inflate.js z-worker.js
-    grunt.registerTask("build", ["copy:main", "concat:libcss" , "cssmin:minify_css" , "uglify:minify_js"]);
+    grunt.registerTask("build", ["copy:main", "concat:libcss" , "cssmin:minify_css" , "uglify:minify_js", "copy:copyToSample"]);
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
